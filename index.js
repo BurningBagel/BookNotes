99a5d07f6f77c668d18debb35f05d7d6b9939941
Luiz Duarte <luiz.duarte11@yahoo.com.br>
2024-04-04 17:49:20 -0300

    correct type in package.json, add main .ejs layout and get route. Add experiment with show more, but will probably just use a new template for notes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
+const port = 3000;
 
 const db = new pg.Client({
     user: 'postgres',
@@ -13,7 +14,17 @@ const db = new pg.Client({
   });
   db.connect();
 
+app.get("/", async (req,res) =>{
 
+    const books = await db.query("SELECT * FROM books;");
+
+    res.render("index.ejs",{books:books.rows});
+});
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
