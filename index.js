9a0222ab7dcf7e70162a59745d7e7143b0db2987
Luiz Duarte <luiz.duarte11@yahoo.com.br>
2024-04-05 15:07:20 -0300

    add plan. Gonna leave this project on ice until im done with the course because what I wanna do with it is gonna take a whiiiiiiile)

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+
+
+/**
+ * THE PLAN
+ *  
+ * This is meant to be my registry of characters, events, factions and locales.
+ *  Characters should have Name, Place of Origin, "Occupation", Affiliation(Rand Al'Thor, The Two Rivers, The Dragon Reborn, Dragonsworn)
+ *  Events should have a Title, Location, Possible date, keys to related characters (Death of Lews Therin, Dragonmount, thousands of years ago, references Rand Al'Thor, Shai'tan, Lews Therin Telamon)
+ *  Factions should have a Name, Location, Description, and keys to those inside the faction (Black Tower, Black Tower, Group of saidin users preparing themselves for Tarmon Gaidon as well as avoiding persecution from the Aes Sedai by teaching each other, Rand Al'Thor, the chief guy i forgot his name fuck i need this database)
+ *  Locales should have a Name, Location relative to other places, Description, and keys to those related to it (Black Tower, Outskirts of Caemlyn(?), Previously known as The Farm, the Black Tower is in fact an abandoned farm that Rand appropriated as the headquarters for his army of saidin users, Rand Al'Thor, chief guy)
+ * 
+ * 
+ * MAN THIS PLAN BALLOONED IN FRONT OF MY EYES
+ * honestly this is shaping up to be quite something, but its gonna take a while to do. I'm gonna leave this as it is for now and finish it after the course.
+ * 
+ * 
+ * 
+ */
+
+
+
 const app = express();
 const port = 3000;
 
